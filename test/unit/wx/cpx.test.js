396425f76e2550a9d157eda37dde8e69b3e8c33e
wangzhiliang <wangzhiliang@didichuxing.com>
2019-01-17 13:39:48 +0800

    utest

--- test/unit/wx/cpx.test.js
+++ test/unit/wx/cpx.test.js
@@ -6,29 +6,21 @@ describe('单位转换', function () {
   it('px2cpx 375px', function () {
     const pxNumber = 375;
     const cpxNumber = 750;
-    return cml.px2cpx(pxNumber, cpx => {
-      expect(cpxNumber).to.equal(cpx);
-    });
+    expect(cpxNumber).to.equal(cml.px2cpx(pxNumber));
   })
   it('px2cpx 414px', function () {
     const pxNumber = 414;
     const cpxNumber = 828;
-    return cml.px2cpx(pxNumber, cpx => {
-      expect(cpxNumber).to.equal(cpx);
-    });
+    expect(cpxNumber).to.equal(cml.px2cpx(pxNumber));
   })
   it('cpx2px 750cpx', function () {
     const cpxNumber = 750;
     const pxNumber = 375;
-    return cml.cpx2px(cpxNumber, px => {
-      expect(pxNumber).to.equal(px);
-    });
+    expect(pxNumber).to.equal(cml.cpx2px(cpxNumber));
   })
   it('cpx2px 828cpx', function () {
     const cpxNumber = 828;
     const pxNumber = 414;
-    return cml.cpx2px(cpxNumber, px => {
-      expect(pxNumber).to.equal(px);
-    });
+    expect(pxNumber).to.equal(cml.cpx2px(cpxNumber));
   })
 })
